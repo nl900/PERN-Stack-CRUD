4fe9034b0bcb7a18e610bfbeaeb584206b2f2fb8
Nancy Lee <nancy.lee@alexsolutions.com.au>
2020-12-08 15:58:47 +1100

    add swagger docs

--- express-server/app/routes/index.js
+++ express-server/app/routes/index.js
@@ -3,20 +3,133 @@ const router = express.Router();
 
 const controller = require("../controllers");
 
+/**
+ * @swagger
+ * definitions:
+ *   Note:
+ *     properties:
+ *       title:
+ *         type: string
+ *       description:
+ *         type: string
+ */
 
 // Get all Notes
+/**
+ * @swagger
+ * /api/notes:
+ *   get:
+ *     tags:
+ *       - Notes
+ *     description: Returns all notes
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of notes
+ *         schema:
+ *           $ref: '#/definitions/Note'
+ */
 router.get("/", controller.findAll);
 
 // Get Note by Id
+/**
+ * @swagger
+ * /api/notes/{id}:
+ *   get:
+ *     tags:
+ *       - Notes
+ *     description: Returns a single note
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Note's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single note
+ *         schema:
+ *           $ref: '#/definitions/Note'
+ */
+
 router.get("/:id", controller.findOne);
 
 // Create a new Note
+/**
+ * @swagger
+ * /api/notes:
+ *   post:
+ *     tags:
+ *       - Notes
+ *     description: Creates a new note
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: note
+ *         description: Note object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Note'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+
 router.post("/", controller.create);
 
 // Modify existing Note
+/**
+ * @swagger
+ * /api/Notes/{id}:
+ *   put:
+ *     tags:
+ *       - Notes
+ *     description: Updates a single note
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Note's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: note
+ *         description: Note object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Note'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+
 router.put("/:id", controller.update);
 
 // Delete Note by Id
+/**
+ * @swagger
+ * /api/notes/{id}:
+ *   delete:
+ *     tags:
+ *       - Notes
+ *     description: Deletes a single note
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Note's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete("/:id", controller.delete);
 
 module.exports = router;
--- express-server/server.js
+++ express-server/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
+
+// swagger definition
+var swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.1',
+    description: 'Demonstrating how to desribe a RESTful API with Swagger',
+  },
+  host: 'localhost:8080',
+  basePath: '/',
+};
+
+// options for the swagger docs
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./app/routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
 
 // Bring in the route
 const routes = require("./app/routes");
@@ -14,13 +37,18 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('dist'));
 
 const db = require("./app/models");
 db.sequelize.sync();
 
 app.use("/api/notes", routes);
+
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 // Define PORT
 const PORT = process.env.PORT || 8080;
